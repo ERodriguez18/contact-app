2591fd14c2de8f0d8f6090f12e72db5766987d08
Erick <erisrnb18@gmail.com>
2021-06-11 10:19:29 -0400

    useeffect to keep data pon local storage

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -7,13 +7,12 @@ class AddContact extends React.Component {
     };
         add = (e => {
             e.preventDefault();
-            if (this.state.name === "" && this.state.email === "") {
+            if (this.state.name === "" || this.state.email === "") {
                 alert("All fields are required!");
                 return;
             }
             this.props.addContactHandler(this.state);
             this.setState({name: "", email: "" });
-            console.log(this.state)
         })
     render() {
         return (
