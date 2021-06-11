2820912f515e243b87d5e0bb241803023fa735c9
Erick <erisrnb18@gmail.com>
2021-06-11 11:36:24 -0400

    delete contact function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from "react";
-import './App.css';
+import { uuid } from "uuidv4";
+import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts"
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
   useEffect(() => {
     const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retrieveContacts) setContacts(retrieveContacts);
@@ -24,8 +34,8 @@ function App() {
   return (
     <div className="ui container">
       <Header />
-      <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts} />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
