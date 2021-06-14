36fa47321f1e2db4bca7500b264ae1feb2ef616b
Erick <erisrnb18@gmail.com>
2021-06-14 18:47:22 -0400

    changed baseurl once again

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL:"https://a-contact-management-app.herokuapp.com/contacts",
+    baseURL:"https://a-contact-management-app.herokuapp.com/",
 })
